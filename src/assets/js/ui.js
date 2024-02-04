9fdc15648f03d13eb5277001e79f80b7c265bd03
kik8727 <kik87827@gmail.com>
2024-02-05 00:19:43 +0900

    메인 html/css 세팅

--- src/assets/js/ui.js
+++ src/assets/js/ui.js
@@ -3,6 +3,7 @@ window.addEventListener("DOMContentLoaded", () => {
 });
 window.addEventListener("load", () => {
   layoutFunc();
+  posLayerEvent();
 });
 
   
@@ -176,3 +177,135 @@ window.addEventListener("load", () => {
       }
     }
   }
+
+
+
+
+
+  
+function posLayerEvent(){
+	var posCallBtn = $("[data-poslayer]");
+	var poslayer_z = $(".poslayer_z");
+	
+	$("body").append(poslayer_z);
+
+	
+	
+	posCallBtn.on("click",function(e){
+		var $this = $(this),
+			$t_t = $($this.attr("data-poslayer"));
+		e.preventDefault();
+		posLayerShow($t_t,$this);
+	});
+	poslayer_z.on("click",".layerclose",function(e){
+		e.preventDefault();
+		posLayerHide($(this).parents(".poslayer_z"));
+	});
+
+	$(document).on("click",".btn_psubmit",function(e){
+		e.preventDefault();
+		let thisParent = $(this).parents(".poslayer_z");
+		let targetCols = $(`[data-poslayer='#${thisParent.attr("id")}']`);
+		let activeDate = thisParent.attr("data-date");
+		let activeText = thisParent.find(".pclayer_vlist > li.active").text();
+		if(thisParent.attr("data-date") !== undefined){
+			targetCols.find(".search_form_text_result").html(activeDate);
+			targetCols.addClass("result_mode");
+		}else{
+			targetCols.find(".search_form_text_result").html(activeText);
+			targetCols.addClass("result_mode");
+		}
+		posLayerHide(thisParent);
+	});
+
+	$(document).on("click",".pcv_chk",function(e){
+		e.preventDefault();
+		$(this).parents("li").siblings().removeClass("active");
+		$(this).parents("li").addClass("active");
+	});
+
+	$(document).on("click",function(e){
+		if (!$(e.target).parents("[data-poslayer] , .poslayer_z , .layer_in_control").length && !$(e.target).is("[data-poslayer]") && !$(e.target).is(".layer_in_control")){
+			posLayerHide($(".poslayer_z.active"));
+		}
+	});
+}
+
+function posLayerShow(target,btn){
+	var poslayer_z = $(".poslayer_z");
+	var target = $(target);
+	
+	$("body").append(target);
+	poslayer_z.removeClass("active");
+	target.addClass("active");
+	posLayerPos(target,btn);
+}
+
+function posLayerResize(){
+	var poslayer_z = $(".poslayer_z");
+	if (poslayer_z.length){
+		poslayer_z.each(function(){
+			posLayerResizeAction($(this));
+		});
+	}
+}
+
+function posLayerPos(target,btn){
+	var $target = $(target);
+	var $target_tvitdep = $target.find(".tvitdep_vlist_wrap");
+	var $target_tvitdep_pos = $target_tvitdep.length ? $target_tvitdep.offset().left : 0;
+	var $target_tvitdep_wid = $target_tvitdep.length ? $target_tvitdep.outerWidth() : 0;
+	var $targetWid = $target.length ? $target.outerWidth() : 0;
+	var $btn = $(btn);
+	var $btnIndex = $btn.index();
+	var $btnPosTop = $btn.length ? $btn.offset().top : 0;
+	var $btnPosHeight = $btn.length ? $btn.outerHeight() : 0;
+	var $btnPosLeft = $btn.length ? $btn.offset().left : 0;
+	var $btnWid = $btn.length ? $btn.outerWidth() : 0;
+	var elseMargin = 0;
+	$target.css({"top":"", "left" : "" , "right" : "" , "width" : ""});
+	if ($targetWid + $btnPosLeft > $(window).width()){
+		$target.css({
+			"top": $btnPosTop + $btnPosHeight + 20,
+			"left": "auto",
+			"right" : 20
+		});
+	}else{
+		$target.css({
+			"top": $btnPosTop + $btnPosHeight + 20,
+			"left": $btnPosLeft
+		});
+	}
+}
+
+function posLayerResizeAction(target){
+	var $target = $(target);
+	var $target_tvitdep = $target.find(".tvitdep_vlist_wrap");
+	var $target_tvitdep_pos = $target_tvitdep.length ? $target_tvitdep.offset().left : 0;
+	var $target_tvitdep_wid = $target_tvitdep.length ? $target_tvitdep.outerWidth() : 0;
+	var $targetWid = $target.length ? $target.outerWidth() : 0;
+	var $btn = $("[data-poslayer='#" + $target.attr("id") +"']");
+	var $btnIndex = $btn.index();
+	var $btnPosTop = $btn.length ? $btn.offset().top : 0;
+	var $btnPosHeight = $btn.length ? $btn.outerHeight() : 0;
+	var $btnPosLeft = $btn.length ? $btn.offset().left : 0;
+	var $btnWid = $btn.length ? $btn.outerWidth() : 0;
+	$target.css({"top":"", "left" : "" , "right" : "" , "width" : ""});
+	if ($targetWid + $btnPosLeft > $(window).width()) {
+		$target.css({
+			"top": $btnPosTop + $btnPosHeight + 20,
+			"left": "auto",
+			"right": 20
+		});
+	} else {
+		$target.css({
+			"top": $btnPosTop + $btnPosHeight + 20,
+			"left": $btnPosLeft
+		});
+	}
+}
+
+function posLayerHide(target){
+	var target = $(target) || target;
+	target.removeClass("active");
+}
